c1ad19f6b07eda1021b6d3f4ad21b4cd6b54525d
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-17 16:47:33 +0300

    глава 3

---
+++ Indroducing Python/Part 1. Основы Python/Chapter 3. Числа/numbers.py
@@ -0,0 +1,21 @@
+print(bool(True))
+print(bool(1))
+print(bool(45))
+print(bool(-45))
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+
+million = 1_000_000
+print(million)
+
+value = 65
+print(bin(value))
+print(oct(value))
+print(hex(value))
+
+print(chr(65))
+print(ord('A'))
+
+print(True + 2)
+print(False + 5.0)
