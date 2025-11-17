95170cb6a4c88b4eca9c92e335ca6ad1a3853787
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-17 16:42:39 +0300

    глава 2

---
+++ Indroducing Python/Part 1. Основы Python/Chapter 2. Данные, типы, значения, переменные и имена/data.py
@@ -0,0 +1,24 @@
+a = 7
+b = a
+print(type(a))
+print(type(b))
+print(type(58))
+print(type(99.9))
+print(type('abc'))
+
+y = 5
+x = 12 - y
+print(x)
+
+two = deux = zwei = 2
+print(two)
+print(deux)
+print(zwei)
+
+a = [2, 4, 6]
+b = a
+print(a)
+print(b)
+a[0] = 99
+print(a)
+print(b)
