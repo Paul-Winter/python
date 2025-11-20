737ca0f1437bf27eb1befb956fbc484213c00b89
Paul Winter <zimin.pavel.v@gmail.com>
2025-11-20 16:58:54 +0300

    глава 5

--- Indroducing Python/Part 1. Основы Python/Chapter 5. Текстовые строки/strings.py
+++ Indroducing Python/Part 1. Основы Python/Chapter 5. Текстовые строки/strings.py
@@ -51,3 +51,54 @@ print(name)
 print(name.replace('H', "P"))
 print(name)
 print('P' + name[1:])
+
+print(letters[20:])
+print(letters[12:15])
+print(letters[-3:])
+print(letters[18:-3])
+print(letters[-6:-2])
+print(letters[::7])
+print(letters[4:20:3])
+print(letters[19::4])
+print(letters[:21:3])
+print(letters[-1::-1])
+print(letters[::-1])
+
+print(len(letters))
+empty = ""
+print(len(empty))
+
+tasks = 'get gloves,get mask,give cat vitamins,call ambulance'
+print(tasks.split(','))
+
+crypto_list = ['Yeti', 'Bigfoot', 'Loch Ness Monster']
+crypto_string = ', '.join(crypto_list)
+print('Found and signing book deals: ', crypto_string)
+
+setup = 'a duck goes into a bar...'
+print(setup)
+print(setup.replace('duck', 'marmoset'))
+print(setup)
+print(setup.replace('a ', 'a famous ', 100))
+print(setup.replace('a', 'a famous ', 100))
+
+world = "      earth      "
+print(world)
+print(world.strip())
+print(world.strip(' '))
+print(world.lstrip())
+print(world.rstrip())
+print(world.strip('!'))
+
+blurt = "What the...!!?"
+print(blurt)
+print(blurt.strip('.?!'))
+
+import string
+print(string.whitespace)
+print(string.punctuation)
+print(blurt.strip(string.punctuation))
+prospector = "What in tarnation ...??!!"
+print(prospector)
+print(prospector.strip(string.whitespace + string.punctuation))
+
