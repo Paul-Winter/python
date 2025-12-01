4f4e10ab8bc9089e3a2a0ef9dee3a5358146f800
Paul Winter <zimin.pavel.v@gmail.com>
2025-12-01 09:58:30 +0300

    модуль 2

--- Код будущего/Модуль 2/2.1 Практика.py
+++ Код будущего/Модуль 2/2.1 Практика.py
@@ -6,5 +6,9 @@ import os
 # print(file.read())
 # file.close()
 
+# 1. запросить у пользователя строку
+# 2. добавить введённую пользователем строку
+#    в файл
+
 with open("test2.txt") as file:
     print(file.read())
