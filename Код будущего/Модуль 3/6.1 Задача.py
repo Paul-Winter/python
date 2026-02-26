57a3f7497ea89dabd1845f19851f585caf01b928
Paul Winter <zimin.pavel.v@gmail.com>
2026-02-26 10:09:17 +0300

    задачи шестой темы

---
+++ Код будущего/Модуль 3/6.1 Задача.py
@@ -0,0 +1,9 @@
+# mkdir my_project
+# cd my_project
+
+# git init
+# git add script.py
+# git commit -m "Добавлен скрипт"
+# git add script.py
+# git commit -m "Добавлена новая функция"
+# git log
---
+++ Код будущего/Модуль 3/6.2 Задача.py
@@ -0,0 +1,7 @@
+# git branch feature
+# git checkout feature
+# git add script.py
+# git commit -m "Добавлена функция test"
+# git checkout main
+# git merge feature
+# git branch -d feature
